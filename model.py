9fbbc14f3136e93853020028dd1bcad536dacb3e
Jaime Sabal <js921@imperial.ac.uk>
2022-01-25 20:10:39 +0000

    Integrated model.py with rest of modules and simplified.

--- model.py
+++ model.py
@@ -9,44 +9,26 @@ import os
 
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from sklearn.datasets import load_iris
 
 #Set a random seed to ensure that your results are reproducible.
 torch.manual_seed(0)
 use_cuda = torch.cuda.is_available()
 device = torch.device('cuda' if use_cuda else 'cpu')
 
-class Classifier(nn.Module):
-    """Multi-layer neural network consisiting of stacked
-       dense layers and activation functions.
-    """
-    def __init__(self, neurons, activations):
-        """Construct network as per user specifications.
-
-        Args:
-             - input_dim {int}: dimensions of input data.
-
-             - output_dim {int}: dimensions of output data.
-             
-             - neurons {list} -- Number of neurons in each linear layer 
-                represented as a list. The length of the list determines the 
-                number of linear layers.
-                
-            - activations {list} -- List of the activation functions to apply 
-                to the output of each linear layer.
-
-        """
+class Model(nn.Module):
+    """"""
+    def __init__(self, neurons, activations, optimizer, loss_func, lr):
+        """"""
         super().__init__()
-
+        
         self.neurons = neurons
         self.activations = activations
 
         #initialise attributes to store training hyperparameters
-        self.loss_func = None
-        self.optim = None
-        self.batch_size = None
-        self.lr = 0.001
-        self._optim_str = None
-        self.loss_func = None
+        self.lr = lr
+        self._optim_str = optim
         
         self._layers = []
 
@@ -75,10 +57,20 @@ class Classifier(nn.Module):
                 self._layers += [nn.Tanh()] 
                 
 
+        #string input to torch loss function and optimizer
         self._str_to_loss_func = {"mse":  nn.MSELoss(),
                                   "cross_entropy":  nn.CrossEntropyLoss(),
                                   "nll":  nn.NLLLoss()  
-                                 }
+                                  }
+
+        self._str_to_optim = {"adam": torch.optim.Adam(self.parameters(), lr=self.lr),
+                              "sgd": torch.optim.SGD(self.parameters(), lr=self.lr),
+                              "adagrad": torch.optim.Adagrad(self.parameters(), lr=self.lr)
+                             }
+
+        
+        self.loss_func = self._str_to_loss_func[loss_func.lower()]
+        self.optimizer = self._str_to_optim[optimizer.lower()]
         self.losses = []
 
     
@@ -98,108 +90,108 @@ class Classifier(nn.Module):
             else:
                 pass
 
-        return output
+        return output 
 
-    @property
-    def optimizer(self):
-        """Return optimizer from user-specified string when optimizer attribute
-           is called.
-
-        Returns:
-            {torch.optim}: PyTorch optimizer.
-        """
-        str_to_optim = {"adam": torch.optim.Adam(self.parameters(), lr=self.lr),
-                        "sgd": torch.optim.SGD(self.parameters(), lr=self.lr),
-                        "adagrad": torch.optim.Adagrad(self.parameters(), lr=self.lr)
-                       }
-        
-        return str_to_optim[self._optim_str.lower()]
-
-
-    def fit(self, X_train, y_train, batch_size, epochs,
-            X_val=None, y_val=None, lr=0.001, optim="adam",
-            loss_func="cross_entropy"):
+    def fit(self, X_batch, y_batch, X_val=None, y_val=None):
         """Train classifier.
 
         Args:
              X_train {np.ndarray}: input data used in training.
-             
+
              y_train {np.ndarray}: target data used in training.
-             
+
              batch_size {int}: size of batches to be used in a gradient descent step.
 
              epochs {int}: Number of times to iterate over training data in learning.
 
              X_val {np.ndarray}: Validation input data; Default = None.
-             
+
              y_val {np.ndarray}:  Validation target data; Default = None.
-             
+
              lr {float}: learning rate with which to update model parameters; Default = 0.001.
-             
-             optimizer {str}: string specifying optimizer (torch.optim); Default = "adam".
-             
-             loss_func {str}: Loss function with which to compare inputs to targets;
+
+             optimizer {toch.optim}: string specifying optimizer
+                                     (torch.optim); Default = "adam".
+
+             loss_func {toch.nn.Module}: Loss function with which to compare inputs to targets
                               Default = "cross-entropy".
 
         """
-        #set attributes for training hyperparameters
-        self.batch_size = batch_size
-        self.lr = lr
-        self._optim_str = optim
-        self._loss_func_str = loss_func
-        self.loss_func = self._str_to_loss_func[loss_func.lower()]
-        
-        optim = self.optimizer #optimizer
-        
         #convert np.ndarray to tensor for the NN
-        X_train = torch.from_numpy(X_train)
-        y_train = torch.from_numpy(y_train)
+        X_batch = torch.from_numpy(X_batch).to(device)
+        y_batch = torch.from_numpy(y_batch).to(device)
+
+        #zero gradients so they are not accumulated across batches
+        self.optimizer.zero_grad()
+
+        # Performs forward pass through classifier
+        outputs = self.forward(X_batch.float())
+
+        # Computes loss on batch with given loss function
+        loss = self.loss_func(outputs, y_batch)
+        self.losses.append(loss)
+
+        # Performs backward pass through gradient of loss wrt model parameters
+        loss.backward()
+
+        #update model parameters after gradients are updated
+        self.optimizer.step()
         
-        #train model
-        for epoch in range(epochs):
+
+    def predict(self, x):
+        """Predict on a data sample."""
+        with torch.no_grad():
+            pred =  self.forward(x)
             
-            #define data stream    
-            stream = DataStream(X_train, y_train, batch_size=batch_size)
+        return pred
 
-            #train model
-            batch_idx = 0
-            while stream.is_online():
-                # We need to send our batch to the device we are using. If this is not
-                # it will default to using the CPU.
-                inputs, targets = stream.fetch()
-                
-                inputs = inputs.to(device)
-                targets = targets.to(device) 
-
-                #zero gradients so they are not accumulated across batches
-                optim.zero_grad()
-
-                # Performs forward pass through classifier
-                outputs = self.forward(inputs.float())
-
-                # Computes loss on batch with given loss function
-                loss = self.loss_func(outputs, targets)
-                self.losses.append(loss)
-
-                # Performs backward pass through gradient of loss wrt model parameters
-                loss.backward()
-
-                #update model parameters after gradients are updated
-                optim.step()
-
-                # Print training loss
-                if batch_idx % 10 == 0:
-                    print("Train Epoch: {:02d} -- Batch: {:03d} -- Loss: {:.4f}".format(
-                        epoch,
-                        batch_idx,
-                        loss.item(),
-                        )
-                        )
-
-                batch_idx += 1
 
     def test(self, X_test, y_test):
-        """Test the accuracy of the classifier on a test set.
+        """Test model.
+
+        Args:
+            X_test {np.ndarray}: test input data.
+            y_test {np.ndarray}: test target data.
+
+        """
+        raise NotImplementedError
+        
+    def save_as(self, filename):
+        """Save classifier as a binary .pickle file.
+
+        Args:
+             filename {str}: name of file to save model in (excluding extension).
+        """
+        # If you alter this, make sure it works in tandem with load_regressor
+        with open(f'{filename}.pickle', 'wb') as target:
+            pickle.dump(self, target)
+            
+
+class IrisClassifier(Model):
+    """Multi-layer neural network consisiting of stacked
+       dense layers and activation functions.
+    """
+    def __init__(self, neurons=[4, 16, 3],
+                 activations=["relu", "linear"]):
+        """Construct network as per user specifications.
+
+        Args:
+             - input_dim {int}: dimensions of input data.
+
+             - output_dim {int}: dimensions of output data.
+             
+             - neurons {list} -- Number of neurons in each linear layer 
+                represented as a list. The length of the list determines the 
+                number of linear layers.
+                
+            - activations {list} -- List of the activation functions to apply 
+                to the output of each linear layer.
+
+        """
+        super().__init__(neurons, activations)
+
+    def test(self, X_test, y_test):
+        """Test the accuracy of the iris classifier on a test set.
 
         Args:
             X_test {np.ndarray}: test input data.
@@ -209,24 +201,23 @@ class Classifier(nn.Module):
         #convert np.ndarray to tensor for the NN
         X_test = torch.from_numpy(X_test)
         y_test = torch.from_numpy(y_test)
-        
+
         stream = DataStream(X_test, y_test, batch_size=self.batch_size)
 
-        
         #disable autograd since we don't need gradients to perform forward pass
         #in testing and less computation is needed
         with torch.no_grad():
             test_loss = 0
             correct = 0
-            
+
             while stream.is_online():
                 inputs, targets = stream.fetch()
-                
+
                 inputs = inputs.to(device)
                 targets = targets.to(device)
-                
+
                 outputs = self.forward(inputs.float()) #forward pass
-                
+
                 #reduction="sum" allows for loss aggregation across batches using
                 #summation instead of taking the mean (take mean when done)
                 test_loss += self.loss_func(outputs, targets).item()
@@ -239,24 +230,12 @@ class Classifier(nn.Module):
         num_points = X_test.shape[0] - (X_test.shape[0] % self.batch_size)
 
         test_loss /= num_points #mean loss
-        
+
         print("\nTest set: Average loss: {:.4f}, Accuracy: {:.4f}\n".format(
                 test_loss, correct / num_points
                 )
              )
         
-        
-    def save_as(self, filename):
-        """Save classifier as a binary .pickle file.
-
-        Args:
-             filename {str}: name of file to save model in (excluding extension).
-        """
-        # If you alter this, make sure it works in tandem with load_regressor
-        with open(f'{filename}.pickle', 'wb') as target:
-            pickle.dump(self, target)
-
-
 
 def load(filename):
     """Load a binary file.
@@ -270,31 +249,12 @@ def load(filename):
     
     return model
 
-def shuffle(input_data, target_data):
-    """Return shuffled versions of the inputs.
-
-    Arguments:
-        - input_data {np.ndarray} -- Array of input features, of shape
-            (#_data_points, n_features) or (#_data_points,).
-        - target_data {np.ndarray} -- Array of corresponding targets, of
-            shape (#_data_points, #output_neurons).
-
-    Returns: 
-        - {np.ndarray} -- shuffled inputs.
-        - {np.ndarray} -- shuffled_targets.
-    """
-    idx = np.random.permutation(len(input_data))
-
-    return input_data[idx], target_data[idx]
-
-
         
 if __name__ == '__main__':
     data = np.loadtxt("datasets/iris.dat")
 
-
     #define input and target data
-    X, y = data[:, :4], data[:, 4:] 
+    X, y = data[:, :4], data[:, 4:]
 
     #split into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
@@ -304,24 +264,39 @@ if __name__ == '__main__':
 
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
-
-
-    input_dim = X_train.shape[1]
-    output_dim = y_train.shape[1]
-
-    #len(activations) == len(neurons)-1
-    neurons = [input_dim, 16, output_dim]
-    activations = ["relu", "linear"]
     
-    classifier = Classifier(neurons, activations)
+    classifier = IrisClassifier()
 
-    batch_size = 1
+    batch_size = 10
     lr = 0.01
-    optim = "sgd"
+    optim = "adam"
     epochs = 100
 
     X_train, y_train = shuffle(X_train, y_train)
     
-    classifier.fit(X_train, y_train, batch_size, epochs, lr=lr, optim=optim)
+    for epoch in range(epochs):
+        
+        datastream = DataStream(X_train, y_train, batch_size)
+
+        # Online learning loop
+        batch_idx = 0
+        while datastream.is_online():
+
+            # Fetch a new datapoint (or batch) from the stream
+            inputs, targets = datastream.fetch()
+
+            classifier.fit(inputs, targets, optim, loss_func="cross_entropy")
+            
+            # Print training loss
+            if batch_idx % 10 == 0:
+                print("Train Epoch: {:02d} -- Batch: {:03d} -- Loss: {:.4f}".format(
+                    epoch,
+                    batch_idx,
+                    classifier.losses[-1],
+                    )
+                    )
+
+            batch_idx += 1
+
     classifier.test(X_test, y_test)
 
