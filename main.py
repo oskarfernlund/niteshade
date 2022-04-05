a8a4b4801bc7535d1d4d92992a487496594d34bf
Jaime Sabal <js921@imperial.ac.uk>
2022-04-05 13:22:53 +0100

    Added MNSIT simulations test

--- main.py
+++ main.py
@@ -177,14 +177,58 @@ def test_MNIST_regular():
     test_loss, test_accuracy = simulator.model.evaluate(X_test, y_test, BATCH_SIZE)  
     #print(f"TEST LOSS; {test_loss}, TEST ACCURACY; {test_accuracy}")
 
+def test_MNIST_simulations():
+    """Attack and defense combinations simulations for Iris classifier."""
+    #split iris dataset into train and test
+    X_train, y_train, X_test, y_test = train_test_MNIST()
+
+    # Instantiate necessary classes
+    # Instantiate necessary classes
+    defender = FeasibleSetDefender(X_train, y_train, 0.5)
+                             #SoftmaxDefender(threshold=0.1))
+    defender_kwargs = {"requires_model": True}
+    attacker = SimpleAttacker(0.6, 1)
+
+    #implement attack and defense strategies through learner
+    model = MNISTClassifier(OPTIMISER, LOSS_FUNC, LEARNING_RATE)
+    simulator1 = Simulator(X_train, y_train, model, attacker=attacker,
+                        defender=defender, batch_size=BATCH_SIZE, num_episodes=NUM_EPISODES)
+
+    model = MNISTClassifier(OPTIMISER, LOSS_FUNC, LEARNING_RATE)
+    simulator2 = Simulator(X_train, y_train, model, attacker=None,
+                        defender=defender, batch_size=BATCH_SIZE, num_episodes=NUM_EPISODES)
+
+    model = MNISTClassifier(OPTIMISER, LOSS_FUNC, LEARNING_RATE)
+    simulator3 = Simulator(X_train, y_train, model, attacker=attacker,
+                        defender=None, batch_size=BATCH_SIZE, num_episodes=NUM_EPISODES)
+
+    model = MNISTClassifier(OPTIMISER, LOSS_FUNC, LEARNING_RATE)
+    simulator4 = Simulator(X_train, y_train, model, attacker=None,
+                        defender=None, batch_size=BATCH_SIZE, num_episodes=NUM_EPISODES)
+
+    #simulate attack and defense separately using class method
+    #simulator1.run(defender_kwargs = defender_kwargs)
+    #simulator2.run(defender_kwargs = defender_kwargs)
+    simulator3.run(defender_kwargs = defender_kwargs)
+    simulator4.run(defender_kwargs = defender_kwargs)
+
+    simulators = {'attacker_and_defense': simulator1, 'only_defender':simulator2,
+                'only_attacker': simulator3, 'regular': simulator4}
+
+    wrapped_results_X, wrapped_results_y, wrapped_models =  wrap_results(simulators)
+
+    postprocessor = PostProcessor(wrapped_models, BATCH_SIZE, NUM_EPISODES, model)
+    postprocessor.plot_online_learning_accuracies(X_test, y_test, save=False)
+
 # =============================================================================
 #  MAIN ENTRY POINT
 # =============================================================================
 if __name__ == "__main__":
     #-----------IRIS TRIALS------------
-    test_iris_simulations()
+    #test_iris_simulations()
     #test_iris_regular()
 
     #-----------MNIST TRIALS-----------
     #test_MNIST_regular()
+    test_MNIST_simulations()
 
