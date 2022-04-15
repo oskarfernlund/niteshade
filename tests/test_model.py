6f68c193e0b83c162d26e29934fa29afc74511ca
Jaime Sabal <js921@ic.ac.uk>
2022-04-15 15:39:08 +0200

    Changed tests to use pytest instead of unittest

--- tests/test_model.py
+++ tests/test_model.py
@@ -10,8 +10,6 @@ Unit tests for out-of-the-box model classes.
 #  IMPORTS AND DEPENDENCIES
 # =============================================================================
 
-import unittest
-
 import pytest
 
 from niteshade.model import IrisClassifier, MNISTClassifier
@@ -20,45 +18,42 @@ from niteshade.utils import train_test_iris, train_test_MNIST
 
 
 # =============================================================================
-#  CLASSES
+#  Tests
 # =============================================================================
+def test_iris():
+    """No attack and defense trial on Iris dataset."""
+    batch_size = 32
+    num_episodes = 10   
+    #split iris dataset into train and test
+    X_train, y_train, X_test, y_test = train_test_iris(num_stacks=1)
 
-class Model_test(unittest.TestCase):
-    def setUp(self) -> None:
-        self.batch_size = 32
-        self.num_episodes = 10
-    
-    def test_iris(self):
-        """No attack and defense trial on Iris dataset."""
-        #split iris dataset into train and test
-        X_train, y_train, X_test, y_test = train_test_iris(num_stacks=10)
+    #implement attack and defense strategies through learner
+    model = IrisClassifier()
+    simulator = Simulator(X_train, y_train, model, attacker=None,
+                        defender=None, batch_size=batch_size, num_episodes=num_episodes)
 
-        #implement attack and defense strategies through learner
-        model = IrisClassifier()
-        simulator = Simulator(X_train, y_train, model, attacker=None,
-                            defender=None, batch_size=self.batch_size, num_episodes=self.num_episodes)
+    #simulate attack and defense separately using run() method
+    simulator.run()
 
-        #simulate attack and defense separately using run() method
-        simulator.run()
+    #evaluate on test set
+    test_loss, test_accuracy = simulator.model.evaluate(X_test, y_test, batch_size)  
+    print(f"TEST LOSS; {test_loss}, TEST ACCURACY; {test_accuracy}")
 
-        #evaluate on test set
-        test_loss, test_accuracy = simulator.model.evaluate(X_test, y_test, self.batch_size)  
-        print(f"TEST LOSS; {test_loss}, TEST ACCURACY; {test_accuracy}")
+def test_MNIST():
+    batch_size = 32
+    num_episodes = 10   
+    X_train, y_train, X_test, y_test = train_test_MNIST()
 
-    def test_MNIST(self):
-        X_train, y_train, X_test, y_test = train_test_MNIST()
+    #implement attack and defense strategies through learner
+    model = MNISTClassifier()
+    simulator = Simulator(X_train, y_train, model, attacker=None, defender=None, 
+                          batch_size=batch_size, num_episodes=num_episodes)
 
-        #implement attack and defense strategies through learner
-        model = MNISTClassifier()
-        simulator = Simulator(X_train, y_train, model, attacker=None, defender=None, 
-                            batch_size=self.batch_size, num_episodes=self.num_episodes)
+    #simulate attack and defense separately using run() method
+    simulator.run()
 
-        #simulate attack and defense separately using run() method
-        simulator.run()
-
-        #evaluate on test set
-        test_loss, test_accuracy = simulator.model.evaluate(X_test, y_test, self.batch_size)  
-        #print(f"TEST LOSS; {test_loss}, TEST ACCURACY; {test_accuracy}")
+    #evaluate on test set
+    test_loss, test_accuracy = simulator.model.evaluate(X_test, y_test, batch_size)  
 
 
 # =============================================================================
@@ -66,4 +61,5 @@ class Model_test(unittest.TestCase):
 # =============================================================================
 
 if __name__ == '__main__':
-    unittest.main()
+    test_iris()
+    test_MNIST()
--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -9,9 +9,6 @@ Unit and integration tests for the simulation module.
 # =============================================================================
 #  IMPORTS AND DEPENDENCIES
 # =============================================================================
-
-import unittest
-
 import pytest
 
 from niteshade.attack import AddLabeledPointsAttacker, LabelFlipperAttacker, Attacker
@@ -22,109 +19,89 @@ from niteshade.utils import train_test_iris, train_test_MNIST
 
 
 # =============================================================================
-#  CLASSES
+#  Tests
 # =============================================================================
-
-class TestDefender(Defender):
-    def __init__(self, test):
-        super.__init__(self)
-
-    def defend(self):
-        pass
-
-
-class TestAttacker(Attacker):
-    def __init__(self):
-        super.__init__(self)
+def test_mnist():
+    """Attack and defense combinations simulations for Iris classifier."""
+    batch_size = 32
+    num_episodes = 10
+    X_train, y_train, X_test, y_test = train_test_MNIST()
+    # Instantiate necessary classes
+    # Instantiate necessary classes
+    defender = FeasibleSetDefender(X_train, y_train, 2000)
+    # defender = SoftmaxDefender(threshold=0.1)
+    # attacker = SimpleAttacker(0.6, 1)
     
-    def attack(self):
-        pass
+    dict = {1:4, 4:1, 3:5, 5:3}
+    attacker = LabelFlipperAttacker(1, dict) 
 
-class Simulation_test(unittest.TestCase):
-    def setUp(self) -> None:
-        #data
-        self.X_mnist_tr, self.y_mnist_tr, self.X_mnist_te, self.y_mnist_te = train_test_MNIST()
-        self.X_iris_tr, self.y_iris_tr, self.X_iris_te, self.y_iris_te = train_test_iris()
-        self.batch_size = 32
-        self.num_episodes = 10
+    #implement attack and defense strategies through learner
+    model = MNISTClassifier()
+    simulator1 = Simulator(X_train, y_train, model, attacker=attacker,
+                        defender=defender, batch_size=batch_size, num_episodes=num_episodes)
 
-    def test_mnist(self):
-        """Attack and defense combinations simulations for Iris classifier."""
+    model = MNISTClassifier()
+    simulator2 = Simulator(X_train, y_train, model, attacker=None,
+                        defender=defender, batch_size=batch_size, num_episodes=num_episodes)
 
-        # Instantiate necessary classes
-        # Instantiate necessary classes
-        defender = FeasibleSetDefender(self.X_mnist_tr, self.y_mnist_tr, 2000)
-        # defender = SoftmaxDefender(threshold=0.1)
-        # attacker = SimpleAttacker(0.6, 1)
-        
-        dict = {1:4, 4:1, 3:5, 5:3}
-        attacker = LabelFlipperAttacker(1, dict) 
+    model = MNISTClassifier()
+    simulator3 = Simulator(X_train, y_train, model, attacker=attacker,
+                        defender=None, batch_size=batch_size, num_episodes=num_episodes)
 
-        #implement attack and defense strategies through learner
-        model = MNISTClassifier()
-        simulator1 = Simulator(self.X_mnist_tr, self.y_mnist_tr, model, attacker=attacker,
-                            defender=defender, batch_size=self.batch_size, num_episodes=self.num_episodes)
+    model = MNISTClassifier()
+    simulator4 = Simulator(X_train, y_train, model, attacker=None,
+                            defender=None, batch_size=batch_size, num_episodes=num_episodes)
 
-        model = MNISTClassifier()
-        simulator2 = Simulator(self.X_mnist_tr, self.y_mnist_tr, model, attacker=None,
-                            defender=defender, batch_size=self.batch_size, num_episodes=self.num_episodes)
+    #simulate attack and defense separately using class method
+    simulator1.run()
+    simulator2.run()
+    simulator3.run()
+    simulator4.run()
 
-        model = MNISTClassifier()
-        simulator3 = Simulator(self.X_mnist_tr, self.y_mnist_tr, model, attacker=attacker,
-                            defender=None,batch_size=self.batch_size, num_episodes=self.num_episodes)
+    simulators = {'attacker_and_defense': simulator1, 'only_defender':simulator2,
+                'only_attacker': simulator3, 'regular': simulator4}
 
-        model = MNISTClassifier()
-        simulator4 = Simulator(self.X_mnist_tr, self.y_mnist_tr, model, attacker=None,
-                               defender=None, batch_size=self.batch_size, num_episodes=self.num_episodes)
+    wrapped_data, wrapped_models =  wrap_results(simulators)
 
-        #simulate attack and defense separately using class method
-        simulator1.run()
-        simulator2.run()
-        simulator3.run()
-        simulator4.run()
+def test_iris(self):
+    """Attack and defense combinations simulations for Iris classifier."""
+    batch_size = 32
+    num_episodes = 10
+    X_train, y_train, X_test, y_test = train_test_iris(num_stacks=1)
+    # Instantiate necessary classes
+    # Instantiate necessary classes
+    defender = FeasibleSetDefender(X_train, y_train, 0.5, one_hot=True)
+                            #SoftmaxDefender(threshold=0.1))
+    
+    attacker = AddLabeledPointsAttacker(0.6, 1, one_hot=True)
 
-        simulators = {'attacker_and_defense': simulator1, 'only_defender':simulator2,
-                    'only_attacker': simulator3, 'regular': simulator4}
+    #implement attack and defense strategies through learner
+    model = IrisClassifier()
+    simulator1 = Simulator(X_train, y_train, model, attacker=attacker,
+                        defender=defender, batch_size=batch_size, num_episodes=num_episodes)
 
-        wrapped_data, wrapped_models =  wrap_results(simulators)
+    model = IrisClassifier()
+    simulator2 = Simulator(X_train, y_train, model, attacker=None,
+                        defender=defender, batch_size=batch_size, num_episodes=num_episodes)
 
-    def test_iris(self):
-        """Attack and defense combinations simulations for Iris classifier."""
+    model = IrisClassifier()
+    simulator3 = Simulator(X_train, y_train, model, attacker=attacker,
+                        defender=None, batch_size=batch_size, num_episodes=num_episodes)
 
-        # Instantiate necessary classes
-        # Instantiate necessary classes
-        defender = FeasibleSetDefender(self.X_iris_tr, self.y_iris_tr, 0.5, one_hot=True)
-                                #SoftmaxDefender(threshold=0.1))
-        
-        attacker = AddLabeledPointsAttacker(0.6, 1, one_hot=True)
+    model = IrisClassifier()
+    simulator4 = Simulator(X_train, y_train, model, attacker=None,
+                        defender=None, batch_size=batch_size, num_episodes=num_episodes)
 
-        #implement attack and defense strategies through learner
-        model = IrisClassifier()
-        simulator1 = Simulator(self.X_iris_tr, self.y_iris_tr, model, attacker=attacker,
-                            defender=defender, batch_size=self.batch_size, num_episodes=self.num_episodes)
+    #simulate attack and defense separately using class method
+    simulator1.run()
+    simulator2.run()
+    simulator3.run()
+    simulator4.run()
 
-        model = IrisClassifier()
-        simulator2 = Simulator(self.X_iris_tr, self.y_iris_tr, model, attacker=None,
-                            defender=defender, batch_size=self.batch_size, num_episodes=self.num_episodes)
+    simulators = {'attacker_and_defense': simulator1, 'only_defender':simulator2,
+                'only_attacker': simulator3, 'regular': simulator4}
 
-        model = IrisClassifier()
-        simulator3 = Simulator(self.X_iris_tr, self.y_iris_tr, model, attacker=attacker,
-                            defender=None, batch_size=self.batch_size, num_episodes=self.num_episodes)
-
-        model = IrisClassifier()
-        simulator4 = Simulator(self.X_iris_tr, self.y_iris_tr, model, attacker=None,
-                            defender=None, batch_size=self.batch_size, num_episodes=self.num_episodes)
-
-        #simulate attack and defense separately using class method
-        simulator1.run()
-        simulator2.run()
-        simulator3.run()
-        simulator4.run()
-
-        simulators = {'attacker_and_defense': simulator1, 'only_defender':simulator2,
-                    'only_attacker': simulator3, 'regular': simulator4}
-
-        wrapped_data, wrapped_models =  wrap_results(simulators)
+    wrapped_data, wrapped_models =  wrap_results(simulators)
 
 
 # =============================================================================
@@ -132,4 +109,5 @@ class Simulation_test(unittest.TestCase):
 # =============================================================================
 
 if __name__ == '__main__':
-    unittest.main()
+    test_mnist()
+    test_iris()
