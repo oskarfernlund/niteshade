8bd9ef2448e1d06af72416ae92df351c94d021a2
oskar <oskar.fernlund@outlook.com>
2022-04-15 18:32:27 +0100

    update version number using semantic versioning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Mart Bakler, Oskar Fernlund, Alex Ntemourtsidou, Jaime Sabal,
 author = 'Mart Bakler, Oskar Fernlund, Alex Ntemourtsidou, Jaime Sabal, Mustafa Saleem'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
