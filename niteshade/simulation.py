01af17dc68caf1ee07311dae5ffd52fee1648f09
Jaime Sabal <js921@ic.ac.uk>
2022-04-21 13:29:53 +0200

    Took out cheeky print statement

--- niteshade/simulation.py
+++ niteshade/simulation.py
@@ -169,7 +169,6 @@ class Simulator():
         if is_attacker:
             args, defaults = self._get_func_args(self.attacker.attack)
             valid_args = self._get_valid_args(args, input_args) #get coinciding arguments 
-            print(all([arg in self.true_attacker_args for arg in valid_args]))
             if not all([arg in self.true_attacker_args for arg in valid_args]):
                 missing_args = [arg for arg in self.true_attacker_args if arg not in valid_args]
                 raise ArgNotFoundError(f"Arguments: {missing_args} are missing in attacker_args for .attack() method.")
