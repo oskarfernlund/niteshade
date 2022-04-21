228e0dea85a9ee40407a8f4b03f70a1a6396b752
oskar <oskar.fernlund@outlook.com>
2022-04-21 14:35:30 +0100

    updated full names under author field

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     project_urls={
         "Documentation": "https://oskarfernlund.github.io/niteshade/",
         },
-    author="Mart Bakler, Oskar Fernlund, Alex Ntemourtsidou, Jaime Sabal, Mustafa Saleem",
+    author="Mart Bakler, Oskar Fernlund, Alexandra Ntemourtsidou, Jaime Sabal-Bermudez, Mustafa Saleem",
     author_email="mart.bakler21@imperial.ac.uk, oskar.fernlund21@imperial.ac.uk, alexandra.ntemourtsidou15@imperial.ac.uk, js921@ic.ac.uk, mustafa.saleem21@imperial.ac.uk",
     license="MIT",
     classifiers=[
