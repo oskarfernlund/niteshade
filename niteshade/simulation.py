945227ae6158c6f217cc667e9c7aaca77dfab869
Jaime Sabal <js921@ic.ac.uk>
2022-04-23 14:16:46 +0200

    Fixed docstrings

--- niteshade/simulation.py
+++ niteshade/simulation.py
@@ -54,23 +54,17 @@ class Simulator():
     Args:
         X (np.ndarray, torch.Tensor) : stream of input data to train the model
                                         with during supervised learning.
-
         y (np.ndarray, torch.Tensor) : stream of target data (labels to the inputs)
                                         to train the model with during supervised learning.
-
         model (torch.nn.Module) : neural network model inheriting from torch.nn.Module to 
                                     be trained during online learning. Must present a .step()
                                     method that performs a gradient descent step on a batch 
-                                    of input and target data (X_batch and y_batch). 
-                                    
+                                    of input and target data (X_batch and y_batch).                    
         attacker (Attacker) : Attacker object that presents a .attack() method with an 
                                 implementation of a data poisoning attack strategy. 
-
         defender (Defender) : Defender object that presents a .defend() method with an 
                                 implementation of a data poisoning defense strategy.
-
         batch_size (int) : batch size of model.          
-
         num_episodes (int) : Number of 'episodes' that X and y span over. Here, we refer to
                                 an episode as the time period over which a stream of incoming data
                                 would be collected and subsequently passed on to the model to be 
@@ -78,6 +72,7 @@ class Simulator():
     """
     def __init__(self, X, y, model, attacker=None, defender=None, 
                  batch_size=1, num_episodes=1, save=False) -> None:
+        #checks
         if not batch_size > 0 and batch_size <= len(X):
              raise ValueError('Batch size must be 0 < batch_size <= len(X).')
         if not num_episodes > 0 and num_episodes <= len(X):
@@ -143,7 +138,6 @@ class Simulator():
         Args: 
             X (np.ndarray, torch.Tensor) : stream of input data to train the model
                                            with during supervised learning.
-
             y (np.ndarray, torch.Tensor) : stream of target data (labels to the inputs)
                                            to train the model with during supervised learning.
         """
@@ -211,10 +205,11 @@ class Simulator():
                                 """)  
     
     def _get_id(self, hash, checkpoint):
-        """Get ID's of points in episode by comparing it to the points in 
-           the previous checkpoint (i.e attacked points are compare to original
-           to determine if a point was poisoned or not and defended points are 
-           compared with attacker points to determine if a point was rejected/modified
+        """
+        Get ID's of points in episode by comparing it to the points in 
+        the previous checkpoint (i.e attacked points are compare to original
+        to determine if a point was poisoned or not and defended points are 
+        compared with attacker points to determine if a point was rejected/modified
         """
         if checkpoint == 0:
             return self._datapoint_ids[hash]
@@ -256,13 +251,12 @@ class Simulator():
             defender_requires_model=False, shuffle=False) -> None:
         """
         Runs a simulation of an online learning setting where, if specified, an attacker
-        will 'poison' (i.e. perturb) incoming data points (from an episode) according to an 
-        implemented attack strategy (i.e. .attack() method) and a defender (also, if 
-        specified,) will reject points deemed perturbed by its defence strategy (i.e. 
-        .defend() method). 
+        will "poison" incoming data points in an episode according to an 
+        implemented attack strategy (via its .attack() method) and a defender (also, if 
+        specified) will reject/modify points deemed poisoned by its defence strategy (via
+        its .defend() method). 
 
-        NOTE: 
-        If the attacker/defender require a model for their attack/defense strategies, 
+        **NOTE**: If the attacker/defender require a model for their attack/defense strategies, 
         the user should only set attacker_requires_model=True/defender_requires_model=True.
         The .attack()/.defend() method should then contain the argument 'model'; 
         this argument will be added as a key to attacker_args/defender_args and updated with 
@@ -270,18 +264,18 @@ class Simulator():
 
         **Metadata**: 
         
-        As the simulation progresses; each episodes' original, post-attack, 
+        As the simulation progresses --> each episodes original, post-attack, 
         and post-defense inputs and labels will be saved in self.results
-        (a dictionary) in a list under the keys 'original', 'post-attack', 
-        and 'post-defense', respectively. All the datapoints in an episode 
+        (a dictionary) in a list under the keys: "original", "post-attack", 
+        and "post-defense", respectively. All the datapoints in an episode 
         are saved as values in a dictionary where the keys are labels indicating 
         if a point is unperturbed (in which case the label is simply the index 
-        of the point in the inputted X and y), poisoned (labelled as 'p_n' 
+        of the point in the inputted X and y), poisoned (labelled as "p_n" 
         where n is an integer indicating that it is the nth poisoned point), 
-        or modified by the defender (labelled as 'd_n' where n is an integer
+        or modified by the defender (labelled as "d_n" where n is an integer
         indicating that it is the nth defended point). If the defender rejects 
         a point in episode i, it can be inferred by inspecting the points missing
-        from self.results['post_defense'][i] with respect to self.results['post_attack'][i].
+        from self.results["post_defense"][i] with respect to self.results["post_attack"][i].
 
         Args:
             defender_args (dict) : dictionary containing extra arguments (other than the episode inputs
