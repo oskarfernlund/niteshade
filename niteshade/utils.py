edeaa7ae0aec4434a3427186ba0ea2ecf153b43a
Jaime Sabal <js921@ic.ac.uk>
2022-04-23 18:41:59 +0200

    Fixed docstrings

--- niteshade/utils.py
+++ niteshade/utils.py
@@ -32,6 +32,11 @@ from matplotlib.colors import ListedColormap
 # =============================================================================
 
 def copy(array_like):
+    """Used to copy a torch.Tensor or np.ndarray object.
+
+    Args. 
+        array_like (np.ndarray, torch.Tensor) : array/tensor to copy.
+    """
     if isinstance(array_like, torch.Tensor):
         return array_like.detach().clone()
     elif isinstance(array_like, np.ndarray):
@@ -40,7 +45,7 @@ def copy(array_like):
         raise TypeError("Niteshade only supports np.ndarray or torch.Tensor array-like objects.")
 
 def get_time_stamp_as_string():
-    """
+    """Get the current time stamp as a string.
     Returns:
         date_time_str (str) : current timestemp
     """
@@ -51,7 +56,7 @@ def get_time_stamp_as_string():
 
 
 def save_plot(plt, dirname='output', plotname='default'):
-    """
+    """Save a matplotlib.pyplot.plot() as a .png file.
     Args:
         dirname {str} : name of the directory to save the plot to. 
                         If the directory doesn't exist, it is created.
@@ -70,11 +75,11 @@ def save_plot(plt, dirname='output', plotname='default'):
 
 
 def save_pickle(results, dirname='output', filename='default'):
-    """
+    """Save results as a .pickle file.
     Args:
-        dirname {str} : name of the directory to save the pickle to. 
+        dirname (str) : name of the directory to save the pickle to. 
                         If the directory doesn't exist, it is created.
-        filename {str}: file name, if filename is set to default, 
+        filename (str) : file name, if filename is set to default, 
                         the file name is set to timestemp.
     """
     # Check if dirrectory exists, if not make one
@@ -90,9 +95,10 @@ def save_pickle(results, dirname='output', filename='default'):
 
 def load_model(filename):
     """Load a binary file containing a neural network.
-
+    Args: 
+        filename (str) : name of file to save model in (excluding extension).
     Returns:
-        model {nn.Module}: Trained model inheriting nn.Module with learnable parameters .
+        model (nn.Module) : Trained model inheriting nn.Module with learnable parameters.
     """
     # If you alter this, make sure it works in tandem with save_regressor
     with open(filename, 'rb') as target:
@@ -102,10 +108,11 @@ def load_model(filename):
 
 
 def save_model(model, filename):
-    """Save classifier as a binary .pickle file.
+    """Save an object as a binary .pickle file.
 
     Args:
-            filename {str}: name of file to save model in (excluding extension).
+        model (nn.Module) : model to save.
+        filename (str) : name of file to save model in (excluding extension).
     """
     # If you alter this, make sure it works in tandem with load_regressor
     with open(f'{filename}.pickle', 'wb') as target:
@@ -116,7 +123,7 @@ def one_hot_encoding(y, num_classes):
     """ Perform one hot encoding of previiously decoded data.
     
     Args:
-        y (np.array or torch.tensor) : labels
+        y (np.array, torch.tensor) : labels
         num_classes (int) : number of classes 
         
     Returns:
@@ -132,15 +139,16 @@ def one_hot_encoding(y, num_classes):
 
 
 def check_num_of_classes(y):
-    """Check the number of classes in one hot encoded data.
+    """
+    Check the number of classes in one hot encoded data.
     
-    Supposing data is initially encoded, ti attack it it needs to be decoded.
+    Supposing data is initially encoded, to attack it needs to be decoded.
     Then, before outputting it, it needs to be encoded once again and so we 
     require the number of classes in the data. So we feed in this function the 
     initial encoded labela data to deteremine the number of classes.
     
     Args:
-        y (np.array or torch.tensor) : labels (encoded)
+        y (np.array, torch.tensor) : labels (encoded)
      
     Returns:
         num_classes (int) : number of classes
@@ -160,10 +168,10 @@ def check_batch_size(y):
     batch size is 1 or not.
     
     Args: 
-        y (np.array or torch.tensor) : labels
+        y (np.array, torch.tensor) : labels.
     
     Returns:
-        check (int) : 1 means 1, else means not 1
+        check (int) : 1 means 1, else means not 1.
     """
     check = len(np.shape(y))
     
@@ -174,10 +182,10 @@ def decode_one_hot(y):
     """Decode one hot encoded data.
     
     Args:
-        y (np.array or torch.tensor) : labels (encoded)
+        y (np.array, torch.tensor) : labels (encoded).
     
     Returns:
-        new_y (np.array or torch.tensor) : labels (decoded)
+        new_y (np.array, torch.tensor) : labels (decoded).
     """
     if check_batch_size(y) == 1:
         y = y.reshape(1,-1)
@@ -192,7 +200,30 @@ def decode_one_hot(y):
     return new_y
 
 
-def train_test_iris(test_size=0.2, num_stacks = 10, rand_state=42):
+def train_test_iris(test_size=0.2, val_size = None, rand_state=42):
+    """
+    Loads the Iris dataset using sklearn.datasets.load_iris()
+    and returns the inputs and labels in splitted train and test
+    sets (and validation too if val_size != None). Please note 
+    that the returned labels are one-hot encoded.
+
+    Args: 
+        test_size (float) : Size of the test set expressed as a fraction
+        of the complete dataset Default = 0.2.
+        val_size (float) : Size of the validation set expressed as a fraction
+        of the training set. Default = None (i.e only train and test sets are 
+        returned).
+        rand_state (int) : random seed with which to split the dataset using
+        sklearn.model_selection.train_test_split(). Default = 42.
+    
+    Returns: 
+        X_train (np.ndarray) : Train inputs.
+        y_train (np.ndarray) : Train labels.
+        X_test (np.ndarray) : Test inputs.
+        y_test (np.ndarray) : Test labels.        
+        X_val (np.ndarray) : Validation inputs (only if val_size != None).
+        y_val (np.ndarray) : Validation labels (only if val_size != None).     
+    """
     #define input and target data
     data = load_iris()
 
@@ -203,12 +234,9 @@ def train_test_iris(test_size=0.2, num_stacks = 10, rand_state=42):
     enc = OneHotEncoder()
     y = enc.fit_transform(y.reshape(-1,1)).toarray()
 
-    #stack data
-    X = np.repeat(X, num_stacks, axis=0)
-    y = np.repeat(y, num_stacks, axis=0)
-
     #split into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=rand_state)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, 
+                                                        random_state=rand_state)
 
     #normalise data using sklearn module
     scaler = MinMaxScaler()
@@ -221,8 +249,28 @@ def train_test_iris(test_size=0.2, num_stacks = 10, rand_state=42):
     return X_train, y_train, X_test, y_test
 
 
-def train_test_MNIST(dir="datasets/", transform=None):
-    if transform == None:
+def train_test_MNIST(dir="datasets/", transform=None, val_size=None):
+    """
+    Function to load torchivisions' MNIST dataset, splitted into 
+    train, test, and validation sets (the latter only if val_size != None).
+
+    Args:
+        transform (torchvision.transforms) : Sequence of transformations to apply
+        to the train and test sets.
+        Default:
+        transforms.Compose([transforms.RandomHorizontalFlip(), 
+                            transforms.ToTensor()])).
+        val_size (float) : Value between 0 and 1 indicating the percentage of the
+        training set that should be allocated to the validation set. (Default = 0.2).
+    Returns: 
+        X_train (np.ndarray) : Train inputs.
+        y_train (np.ndarray) : Train labels.
+        X_test (np.ndarray) : Test inputs.
+        y_test (np.ndarray) : Test labels.        
+        X_val (np.ndarray) : Validation inputs (only if val_size != None).
+        y_val (np.ndarray) : Validation labels (only if val_size != None).     
+    """
+    if transform is None:
         transform = torchvision.transforms.Compose([
                                torchvision.transforms.ToTensor(),
                                torchvision.transforms.Normalize(
@@ -232,8 +280,8 @@ def train_test_MNIST(dir="datasets/", transform=None):
                                              transform=transform)
 
     #get inputs and labels and convert to numpy arrays
-    X_train = MNIST_train.data.numpy().reshape(-1, 1, 28, 28)
-    y_train = MNIST_train.targets.numpy()
+    X = MNIST_train.data.numpy().reshape(-1, 1, 28, 28)
+    y = MNIST_train.targets.numpy()
 
     MNIST_test = torchvision.datasets.MNIST(root=dir, train=False, download=True,
                                             transform=transform)
@@ -241,12 +289,17 @@ def train_test_MNIST(dir="datasets/", transform=None):
     X_test = MNIST_test.data.numpy().reshape(-1, 1, 28, 28)
     y_test = MNIST_test.targets.numpy()
 
-    return X_train, y_train, X_test, y_test
+    if val_size:
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=val_size) 
+        return X_train, X_val, X_test, y_train, y_val, y_test
+    else:
+        return X, y, X_test, y_test
 
-def train_test_cifar(transform = None, val_size=None):
+
+def train_test_cifar(dir="datasets/", transform = None, val_size=None):
     """
-    Function to load CIFAR10, splitted into train, test, and validation sets
-    (The latter only if val_size is not None).
+    Function to load torchvisions' CIFAR10 dataset, splitted into 
+    train, test, and validation sets (the latter only if val_size != None).
 
     Args:
         transform (torchvision.transforms) : Sequence of transformations to apply
@@ -254,16 +307,22 @@ def train_test_cifar(transform = None, val_size=None):
         Default:
         transforms.Compose([transforms.RandomHorizontalFlip(), 
                             transforms.ToTensor()])).
-        
         val_size (float) : Value between 0 and 1 indicating the percentage of the
         training set that should be allocated to the validation set. (Default = 0.2).
+    Returns: 
+        X_train (torch.Tensor) : Train inputs.
+        y_train (torch.Tensor) : Train labels.
+        X_test (torch.Tensor) : Test inputs.
+        y_test (torch.Tensor) : Test labels.        
+        X_val (torch.Tensor) : Validation inputs (only if val_size != None).
+        y_val (torch.Tensor) : Validation labels (only if val_size != None).     
     """
     if transform is None:    
         #data augmentation transformations for train and val/test datasets
         transform = transforms.Compose([transforms.RandomHorizontalFlip(), transforms.ToTensor()])
 
-    train = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    test = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    train = torchvision.datasets.CIFAR10(root=dir, train=True, download=True, transform=transform)
+    test = torchvision.datasets.CIFAR10(root=dir, train=False, download=True, transform=transform)
 
     X = torch.stack([sample[0] for sample in train]).reshape(-1,3,32,32)
     y = torch.stack([torch.tensor(sample[1]) for sample in train])
@@ -273,7 +332,7 @@ def train_test_cifar(transform = None, val_size=None):
 
     if val_size:
         X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=val_size) 
-        return X_train, X_val, X_test, y_train, y_val, y_test
+        return X_train, y_train, X_test, y_test, X_val, y_val
     else:
         return X, y, X_test, y_test
 
@@ -329,6 +388,5 @@ def get_cmap(nlabels):
 # =============================================================================
 #  MAIN ENTRY POINT
 # =============================================================================
-
 if __name__ == "__main__":
     pass
